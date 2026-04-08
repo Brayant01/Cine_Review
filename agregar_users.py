77ff43311dbe86691fabede345b5221c801bbedf
Brayant01 <123911814+Brayant01@users.noreply.github.com>
2026-04-08 09:35:08 -0300

    actualizar

--- agregar_users.py
+++ agregar_users.py
@@ -4,7 +4,7 @@ conexion_a_base_de_dados = mysql.connector.connect(
     host = "localhost",
     user = "root",
     password = "1234",
-    database = ""
+    database = "prueba1"
 )
 
 cursor = conexion_a_base_de_dados.cursor()
@@ -21,15 +21,15 @@ def input_validation(valor_max, mensagem):
     
         if  valor > valor_max or valor < 1:
             print("\ningresa um valor correcto")
-            input("presiona cualquier tecla para continuar\n")
+            input("presiona qualquer tecla para continuar\n")
         else:
             return valor
 
-#cria um usuario e agrega el na base de datos si nao existe a tabla crea uma de uma vez e agrega o usuario
+#cria um usuario e agrega ele na base de datos, se nao existe a tabla crea uma de uma vez e agrega o usuario
 def criar_usuario(cursor, admin = 0):
-    nome  = str(input("\nIngresa teu Nome: "))
-    senha = str(input("\nIngresa tua Senha: "))
-    email = str(input("\nIngresa teu Email: "))
+    nome  = str(input("\nIngresa teu Nome: ")).lower
+    senha = str(input("\nIngresa tua Senha: ")).lower
+    email = str(input("\nIngresa teu Email: ")).lower
 
     try:
         query = "INSERT INTO usuario (nome,senha,email,admin) VALUES (%s,%s,%s,%s)"
@@ -37,6 +37,7 @@ def criar_usuario(cursor, admin = 0):
         conexion_a_base_de_dados.commit()
         print("------- Usuario criado ----------")
         print("voltando ao menu inicial favor de fazer login com a opçao 2")
+
     except:
         cursor.execute("CREATE TABLE usuario("
                         "id int primary key auto_increment not null,"
@@ -54,22 +55,36 @@ def criar_usuario(cursor, admin = 0):
 
 
 #valida o login do usuario pasando um de 3 valores
-def normal_user_login(senha,email):
-
-    query = "SELCT senha FROM usuario WHERE email = %s"
-    cursor.execute(query,(email))
+def user_login(senha,email):
+    
+    query = "SELECT senha FROM usuario WHERE email = %s"
+    cursor.execute(query,(email,))
 
     resultado_busqueda = cursor.fetchone()
 
-    if resultado_busqueda: #volta 1 de perfeito cara foi validado
-        if resultado_busqueda[0] == senha:
-            return 1
-        else: #volta 2 de senha invalida
-            print("contraseña incorrecta")
-            return 2
-    else: #Volta 3 do usuario nao existe :C
-        print("El usuario no existe")
-        return 3
+    if resultado_busqueda: #verifica se tem ou nao um registro se nao devolve 0
+
+        if resultado_busqueda[0] == senha: #Se tem um registro verifica o valor com a senha ingresada
+            print("bem vido")
+
+            query = "SELECT admin FROM usuario WHERE email = %s" #verifica o valor de admin
+            cursor.execute(query,(email,))
+
+            resultado_busqueda = cursor.fetchone()
+
+            if resultado_busqueda[0] == 1: #se for admin = 1 entao devolve 2 (usuario admin)
+                print("O usuario é admin")
+                return 2
+            else:                          #se nao entao devolve 1 (usuario normal)
+                print("Usuario nao admin")
+                return 1
+        else:                              # se a senha nao é igaul devolve 0 e uma mensagem
+            print("senha incorrecta")
+            return 0
+    else:
+        print("O usuario nao existe")
+        return 0
+        
             
     
 #---------------------------!!!!so pode o administrador!!!!---------------------------#
@@ -87,33 +102,7 @@ def verificar_bd(cursor):
             print("Base de datos") 
         else: 
             return 2
-
-#valida el login do administrador pasando um de 3 valores
-def login_admin(senha, email):
-    query = "SELECT admin FROM usuario WHERE email = %s"
-    cursor.execute(query,(email))
-
-    resultado_busqueda = cursor.fetchone()
-
-    if resultado_busqueda == 1:
-        query = "SELECT senha FROM usuario WHERE email = %s"
-        cursor.execute(query,(email))
-
-        resultado_busqueda = cursor.fetchone()
-
-        if resultado_busqueda:
-            if resultado_busqueda[0] == senha:
-                return 1
-            else:
-                print("contraseña incorrecta")
-                return 2
-        else:
-            print("El usuario nao existe")
-            return 3
-    else:
-        print("nao tem permisos como administrador")
-        input()
-        return 3       
+     
 
 # ver todas as bases de dados criadas
 def ver_db(cursor):
@@ -133,39 +122,55 @@ while True:
 
     #----------- MENU PRINCIPAL -----------#
     
-    while True:
-        opcion = input_validation(3,"ingresa uma das siguientes opçoens"
-                                 "\nVer filme    - 1"
-                                 "\nfazer login      - 2" 
-                                 "\ncriar uma conta - 3")
+    
+    opcion = input_validation(3,"ingresa uma das siguientes opçoens"
+                             "\nVer filme       - 1"
+                             "\nfazer login     - 2" 
+                             "\ncriar uma conta - 3")
 
-        if opcion   == 1: 
-            print("opcaon em desenvolvimento")
-            break
-        elif opcion == 2: 
-            print("opcaon em desenvolvimento")
-            break
-        elif opcion == 3:
-            print("Escolha uma das siguientes opcoes:\n")
-            print("Administrador  - 1")
-            print("Usuario normal - 2")
+    if opcion   == 1: 
+        print("opcaon em desenvolvimento")
+        break
+    elif opcion == 2: 
+        print("---------------------- LOGIN ----------------------")
+        email = input("EMAIL: ").lower
+        senha = input("SENHA: ").lower
 
-            x = int(input("opçao: "))
+        '''
+        Se o usario for admin entao tem essas opçoes
 
-            if x == 1:
-                print("Para criar uma conta de administrador ingresas as siguientes informacoes:")
-                acceso = input("Acceso admins: ")
-                senha  = input("Senhas admins: ")
+        1- criar filme
+        2- modificar filme
 
-                if acceso == "root" and senha == "123":
-                    criar_usuario(cursor,1)
+        se o usuario nao for admin entao tem essas opçoes
+
+        1- ver filmes
+        2- avaliar filme (seleciona um e da uma nota e um comentario)
+        3- modificar login??
+        '''
+
+        break
+    elif opcion == 3:
+        print("-------------------- CRIAR CONTA ----------------------")
+        print("Escolha uma das siguientes opcoes:\n")
+        print("Administrador  - 1")
+        print("Usuario normal - 2")
+
+        x = int(input("opçao: "))
+
+        if x == 1:
+            print("Para criar uma conta de administrador ingresas as siguientes informacoes:")
+            acceso = input("Acceso admins: ").lower
+            senha  = input("Senhas admins: ").lower
+
+            if acceso == "root" and senha == "123":
+                criar_usuario(cursor,1)
                 
-            else:
-                print("ingresa as siguientes informacoes")
-                criar_usuario(cursor,0)
+        else:
+            print("ingresa as siguientes informacoes")
+            criar_usuario(cursor,0)
             
 
 
-            break
+    break
 
-    print("Voltando ao inicio")
