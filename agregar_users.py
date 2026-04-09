1f54a785b14a2ea5b257c13eef2eb787e93d6f23
Brayant01 <123911814+Brayant01@users.noreply.github.com>
2026-04-09 12:00:44 -0300

    agregar

--- agregar_users.py
+++ agregar_users.py
@@ -25,6 +25,34 @@ def input_validation(valor_max, mensagem):
         else:
             return valor
 
+def create_filme(cursor, classificacao_indicativa):
+    
+    titulo = str(input("\nIngrea o titulo do filme: ")).lower
+    ano_lancamento = str(input("\nIngresa o ano do lançamento: ")).lower
+    sinopse = str(input("\nIngresa uma sinpse: ")).lower
+
+    try:
+        query = "INSERT INTO filme (titulo, ano_lancamente, sinopse, classificacao_indicativa) VALUES (%s,%s,%s,%s)"
+        cursor.execute(query, (titulo,ano_lancamento,sinopse,classificacao_indicativa))
+        conexion_a_base_de_dados.commit()
+        print("-------------- Filme criado --------------")
+        print("voltando ao menu inicial favor de fazer login com a opçao 2")
+    
+    except:
+        cursor.execute("create table filme("
+                       "id int primary key auto_increment not null,"
+                       "titulo varchar(255) not null,"
+                       "ano_lancamento year not null,"
+                       "sinopse text not null,"
+                       "classificacao_indicativa enum('L', '10', '12', '14', '16', '18') not null);")
+        
+        query = "INSERT INTO filme (titulo, ano_lancamente, sinopse, classificacao_indicativa) VALUES (%s,%s,%s,%s)"
+        cursor.execute(query, (titulo,ano_lancamento,sinopse,classificacao_indicativa))
+        conexion_a_base_de_dados.commit()
+        print("-------------- Filme criado --------------")
+        print("voltando ao menu inicial favor de fazer login com a opçao 2")
+
+
 #cria um usuario e agrega ele na base de datos, se nao existe a tabla crea uma de uma vez e agrega o usuario
 def criar_usuario(cursor, admin = 0):
     nome  = str(input("\nIngresa teu Nome: ")).lower
@@ -65,7 +93,7 @@ def user_login(senha,email):
     if resultado_busqueda: #verifica se tem ou nao um registro se nao devolve 0
 
         if resultado_busqueda[0] == senha: #Se tem um registro verifica o valor com a senha ingresada
-            print("bem vido")
+            print("Bem vido")
 
             query = "SELECT admin FROM usuario WHERE email = %s" #verifica o valor de admin
             cursor.execute(query,(email,))
@@ -87,9 +115,9 @@ def user_login(senha,email):
         
             
     
-#---------------------------!!!!so pode o administrador!!!!---------------------------#
+#---------------------------!!!! FUNCOES PARA VER A BASE DE DADOS !!!!---------------------------#
 
-#verificar se pode se crear a base de dados se no proba ver que bases de datos tem no sistema.
+#verificar se pode-se criar a base de dados se no proba ver que bases de datos tem no sistema.
 def verificar_bd(cursor):
     try:
         cursor.execute("CREATE DATABASE IF NOT EXISTS prueba1;")
@@ -121,16 +149,18 @@ while True:
     print("\nSeleciona uma das siguientes opçoes")
 
     #----------- MENU PRINCIPAL -----------#
-    
+
     
     opcion = input_validation(3,"ingresa uma das siguientes opçoens"
                              "\nVer filme       - 1"
                              "\nfazer login     - 2" 
                              "\ncriar uma conta - 3")
-
-    if opcion   == 1: 
+    
+    if opcion   == 1:
+        print("---------------------- VER FILMES ----------------------")
         print("opcaon em desenvolvimento")
         break
+
     elif opcion == 2: 
         print("---------------------- LOGIN ----------------------")
         email = input("EMAIL: ").lower
@@ -141,6 +171,8 @@ while True:
 
         1- criar filme
         2- modificar filme
+        3- criar categoria
+        3- modificar login??
 
         se o usuario nao for admin entao tem essas opçoes
 
